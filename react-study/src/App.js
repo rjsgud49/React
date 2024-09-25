21730b87e361446ffc7f148297f674bce95007a4
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-09-25 13:52:38 +0900

    d

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function Square({ value, onClick }) {
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    );
+}
+
+function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(true);
+
+    const handleClick = (i) => {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        const newSquares = squares.slice();
+        newSquares[i] = isXNext ? "X" : "O";
+        setSquares(newSquares);
+        setIsXNext(!isXNext);
+    };
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = `Winner: ${winner}`;
+    } else {
+        status = `Next player: ${isXNext ? "X" : "O"}`;
+    }
+
+    const renderSquare = (i) => {
+        return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    };
+
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+function resatGame() {
+    <button onClick={resatGame}>Reset</button>;
+    window.location.reload();
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+        </div>
+    );
 }
 
 export default App;
