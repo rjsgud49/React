ad00a07b0334c2bd009d438dc781ccbe37426d04
rjsgud49 <162693556+rjsgud49@users.noreply.github.com>
2024-09-26 19:55:36 +0900

    s

---
+++ react-study/src/projectmodule/Tic Tac Toe/Tic Tac Toe.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import "./Tic Tac Toe.css";
+
+function Square({ value, onClick }) {
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    );
+}
+
+function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(true);
+
+    const handleClick = (i) => {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+
+        const newSquares = squares.slice();
+        newSquares[i] = isXNext ? "X" : "O";
+        setSquares(newSquares);
+        setIsXNext(!isXNext);
+    };
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = `Winner: ${winner}`;
+    } else {
+        status = `Next player: ${isXNext ? "X" : "O"}`;
+    }
+
+    const renderSquare = (i) => {
+        return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    };
+
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+function resatGame() {
+    window.location.reload();
+}
+
+function Game() {
+    return (
+        <button class="reset" onClick={resatGame}>
+            Reset
+        </button>
+    );
+}
+
+function App() {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-reset">
+                <Game />
+            </div>
+        </div>
+    );
+}
+
+export default App;
